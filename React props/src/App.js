007d9c176f07a39163a4c80ce1d64e035f7eba24
safa bouguezzi <62938853+safabouguezzi@users.noreply.github.com>
2022-10-21 00:10:25 +0100

    add react props

---
+++ React props/src/App.js
@@ -0,0 +1,14 @@
+import Profile from './profile/Profile';
+import './App.css';
+
+function App() {
+  return (
+    <div  style={{backgroundColor:"#0A192F"}}>
+      <section style={{minHeight:"100vh"}}>
+          <Profile />
+      </section>
+    </div>
+  );
+}
+
+export default App;
