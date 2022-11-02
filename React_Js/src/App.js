14f01d23f2a3c5e854a6808abb7fabc923c193c9
safa bouguezzi <62938853+safabouguezzi@users.noreply.github.com>
2022-11-02 11:29:30 +0100

    add Hooks

--- React_Js/src/App.js
+++ React_Js/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import defaultimage from './images/background_Images/large.png';
-import Logo from './images/icons/favicon.png';
 
 
 function App() {
@@ -14,21 +13,21 @@ function App() {
           <form >
             <div id="welcome" className="welcome">
 
-              <h1 class="text-1">Every new friend is a new adventure</h1>
-              <p class="text-2">Let's get connected</p>
+              <h1 className="text-1">Every new friend is a new adventure</h1>
+              <p className="text-2">Let's get connected</p>
             </div>
             <div id="div_firstName" className="form-group div_firstName input-box">
-              <i class="fas fa-user"></i>
+              <i className="fas fa-user"></i>
               <input type="text" className="form-control" id="firstName" placeholder="First Name" />
             </div>
 
             <div id="div_lastName" className="form-group div_lastName input-box">
-              <i class="fas fa-user"></i>
+              <i className="fas fa-user"></i>
               <input type="text" className="form-control" id="lastName" placeholder="Last Name" />
             </div>
 
             <div className="form-group div_email input-box">
-              <i class="fas fa-envelope"></i>
+              <i className="fas fa-envelope"></i>
               <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email" />
             </div>
             <div>
@@ -37,12 +36,12 @@ function App() {
             </div>
 
             <div className="form-group div_password input-box">
-              <i class="fas fa-lock"></i>
+              <i className="fas fa-lock"></i>
               <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
             </div>
             <div className="form-check div_checkbox">
               <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-              <label className="form-check-label" for="exampleCheck1">Remember me</label>
+              <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
             </div>
             <button type="submit" className="btn btn-primary div_submit">Submit</button>
           </form>
